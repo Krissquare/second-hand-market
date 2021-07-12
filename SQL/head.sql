f2af75d7a72bf8a2a12fa317c7f2ada9ec808492
YitongGao1016 <2496241579@qq.com>
2021-07-12 16:09:41 +0800

    添加用户图片路径，才可以打开修改用户信息界面

--- SQL/head.sql
+++ SQL/head.sql
@@ -1,7 +1,6 @@
 CREATE TABLE `userHead` (
                             `u_Url` varchar(100),
-                        `u_Account` varchar(100) NOT NULL,
-                        PRIMARY KEY (`u_Account`)
+                            `u_Account` varchar(100) NOT NULL,
+                            PRIMARY KEY (`u_Account`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --
+INSERT INTO `userhead` (`u_Url`, `u_Account`) VALUES ('/images/user/default4.jpg', 'user1');
