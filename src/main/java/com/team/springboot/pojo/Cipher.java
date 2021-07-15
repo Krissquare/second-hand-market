7a551e53e8b9d1776772906d1de81915f104c171
Krissquare <734195698@qq.com>
2021-07-15 15:09:25 +0800

    做到一半的密码加密

---
+++ src/main/java/com/team/springboot/pojo/Cipher.java
@@ -0,0 +1,16 @@
+package com.team.springboot.pojo;
+
+public class Cipher {
+    public static String Encipher(String originPassword){
+        String encipheredPassword = originPassword;
+
+        return encipheredPassword;
+    }
+
+    public static String Decipher(String encipheredPassword){
+        String originPassword = encipheredPassword;
+
+        return originPassword;
+    }
+
+}
